f70eb69fe29378ac1fcd5eb27d48b9f5e892e5fa
j4hangir <j4hangir@gmail.com>
2024-01-13 19:35:32 +0100

    improve declarations

--- vue-tagged-input.d.ts
+++ vue-tagged-input.d.ts
@@ -1,28 +1,55 @@
 // vue-tagged-input.d.ts
 
 declare module '@j4hangir/vue-tagged-input' {
-    import {DefineComponent} from 'vue';
+    import {DefineComponent, PropType} from 'vue';
 
     export interface Tag {
-        text: string;
+        text: string
         style?: string
         classes?: string
     }
 
-    export interface Vue3TagsInputProps {
-        modelValue?: Tag[] | string[] | string;
-        addOnKey?: string[];
-        addOnBlur?: boolean;
-        placeholder?: string;
-        maxTags?: number;
-        validate?: RegExp | ((tag: string) => boolean);
-        disabled?: boolean;
-        readonly?: boolean;
-        removeOnDelete?: boolean;
-        beforeAdding?: (tag: string) => boolean;
-    }
+    export const VueTaggedInput: DefineComponent<{
+        modelValue: {
+            type: PropType<Tag[] | string[] | string>;
+            default: undefined;
+        };
+        addOnKey: {
+            type: PropType<string[]>;
+            default: undefined;
+        };
+        addOnBlur: {
+            type: BooleanConstructor;
+            default: undefined;
+        };
+        placeholder: {
+            type: StringConstructor;
+            default: undefined;
+        };
+        maxTags: {
+            type: NumberConstructor;
+            default: undefined;
+        };
+        validate: {
+            type: PropType<RegExp | ((tag: string) => boolean)>;
+            default: undefined;
+        };
+        disabled: {
+            type: BooleanConstructor;
+            default: undefined;
+        };
+        readonly: {
+            type: BooleanConstructor;
+            default: undefined;
+        };
+        removeOnDelete: {
+            type: BooleanConstructor;
+            default: undefined;
+        };
+        beforeAdding: {
+            type: PropType<(tag: string) => boolean>;
+            default: undefined;
+        };
+    }>
 
-    const VueTagsInput: DefineComponent<Vue3TagsInputProps>;
-
-    export default VueTagsInput;
 }
